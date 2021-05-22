0193c8455b3a0b1116398b1cd78f9dc84670c948
LooperXX <looperxx@outlook.com>
2021-05-22 18:52:01 +0800

    Update

--- static/index.js
+++ static/index.js
@@ -1,15 +1,10 @@
 document.getElementById("build-timestamp").innerText = "Last Update: " + new Date(document.getElementById("build-timestamp").getAttribute("datetime")).toLocaleString();
 
-// Handlebars.registerHelper("cleanTitle", function(title){
-//   return title.slice(0, title.indexOf(("(arXiv:")))
-// })
-
-window.onload=function(){
-  $('.article-expander__title').on('ready', function (event) {
-    var t = event.target;
-    t.textContent = t.textContent.slice(0, t.textContent.indexOf(("(arXiv:")));
-  });
-}
+$('.article-expander__title').replaceWith(function(event) {
+  var t = event.target;
+  t.textContent = t.textContent.slice(0, t.textContent.indexOf(("(arXiv:")));
+  return t
+});
 
 // function cleanTitle(event){
 //   var t = event.target;
