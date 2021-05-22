6d3bf0d52c30ef2fdf261e9e44cd2bd65e843dc9
LooperXX <looperxx@outlook.com>
2021-05-22 14:51:22 +0800

    Update html, hbs and js

---
+++ static/index.js
@@ -0,0 +1,9 @@
+/* Exapand/Collapse with TAB key */
+var expanded = false;
+document.onkeydown=function(e){
+  if(e.keyCode == 9) {
+    expanded = !expanded;
+    document.querySelectorAll("details").forEach(detail => detail.open = expanded);
+    return false;
+  }
+}
