efc87eb239d251f1cad55a85d1a2ba3052cc9333
LooperXX <looperxx@outlook.com>
2021-05-25 13:07:43 +0800

    Update keywords

--- static/index.js
+++ static/index.js
@@ -2,7 +2,7 @@
  * @Author: LooperXX
  * @Email: xxu@ir.hit.edu.cn
  * @Date: 2021-05-22 14:40:50
- * @LastEditTime: 2021-05-25 11:19:36
+ * @LastEditTime: 2021-05-25 11:20:30
  * @LastEditors: LooperXX
  * @Description: 
  */
@@ -34,7 +34,7 @@ document.onkeydown=function(e){
 // Highlight Keywords
 var authors = ["Kaiming He", "Graham Neubig", "Tie-Yan Liu", "Xipeng Qiu", "Yue Zhang", "Christopher Manning", "Noah A. Smith"];
 
-var titles_area = ["Task-oriented", "Dialogue", "Dialog", "Cross-Modal", "Multi-Modal", "Contrastive Learning", "Vision-and-Language"];
+var titles_area = ["Task-oriented", "Dialogue", "Dialog", "Cross-Modal", "Multi-Modal", "Contrastive Learning", "Vision-and-Language", "V&L"];
 var titles_model = ["BERT", "GPT","Transformer"];
 var titles = []
 titles = titles.concat(titles_area,titles_model)
