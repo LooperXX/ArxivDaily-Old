40058f9d98691fb88303dad375dd4e81d0892250
LooperXX <LooperXX@users.noreply.github.com>
2021-06-04 04:58:02 +0000

    deploy: 934e8df78e033978898cf7df991612cf5e4599d5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @Author: LooperXX
  * @Email: xxu@ir.hit.edu.cn
  * @Date: 2021-05-22 14:40:50
- * @LastEditTime: 2021-06-03 11:46:57
+ * @LastEditTime: 2021-06-04 12:55:02
  * @LastEditors: LooperXX
  * @Description: 
  */
@@ -32,7 +32,7 @@ document.onkeydown=function(e){
 }
 
 // Highlight Keywords
-var authors = ["Kaiming He", "Graham Neubig", "Tie-Yan Liu", "Xipeng Qiu", "Yue Zhang", "Christopher Manning", "Noah A. Smith"];
+var authors = ["Kaiming He", "Graham Neubig", "Tie-Yan Liu", "Xipeng Qiu", "Yue Zhang", "Christopher Manning", "Noah A. Smith", "Jason Eisner", "Percy Liang"];
 
 var titles_area = ["Task-oriented", "Conversation", "Dialogue", "Dialog", "Cross-Modal", "Multi-Modal", "Contrastive Learning", "Vision-and-Language", "V&L", "Pre-train", "Pretrain"];
 var titles_model = ["BERT", "GPT","Transformer"];
